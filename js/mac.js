8bca8ab829abfd4553927db465aadfcc24b6c0ef
muzi <1099438829@qq.com>
2019-01-05 20:59:08 +0800

    打开窗口优化

--- js/mac.js
+++ js/mac.js
@@ -800,6 +800,10 @@ window.Win10 = {
         Win10._settop(layero_opened);
         //重新定义菜单布局
         layero_opened.find('.layui-layer-setwin').prepend('<a class="win10-btn-refresh" index="' + index + '" href="#"></a>');
+        //菜单排列倒序
+        $(".layui-layer-setwin>a").each(function(){
+                $(this).prependTo(".layui-layer-setwin");
+        })
         layero_opened.find('.layui-layer-setwin .layui-layer-max').click(function () {
             setTimeout(function () {
                 var height=layero_opened.css('height');
