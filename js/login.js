f61b4b4cad28d945788107bcad02beb1a23b9005
1099438829 <1099438829@qq.com>
2021-01-06 09:47:20 +0800

    提交格式化

--- js/login.js
+++ js/login.js
@@ -1,37 +1,37 @@
-$(document).ready(function() {
-  
+$(document).ready(function () {
+
   var animating = false,
-      submitPhase1 = 1100,
-      submitPhase2 = 400,
-      logoutPhase1 = 800,
-      $login = $(".login"),
-      $app = $(".app");
-  
+    submitPhase1 = 1100,
+    submitPhase2 = 400,
+    logoutPhase1 = 800,
+    $login = $(".login"),
+    $app = $(".app");
+
   function ripple(elem, e) {
     $(".ripple").remove();
     var elTop = elem.offset().top,
-        elLeft = elem.offset().left,
-        x = e.pageX - elLeft,
-        y = e.pageY - elTop;
+      elLeft = elem.offset().left,
+      x = e.pageX - elLeft,
+      y = e.pageY - elTop;
     var $ripple = $("<div class='ripple'></div>");
-    $ripple.css({top: y, left: x});
+    $ripple.css({ top: y, left: x });
     elem.append($ripple);
   };
-  
-  $(document).on("click", ".login__submit", function(e) {
+
+  $(document).on("click", ".login__submit", function (e) {
     if (animating) return;
     animating = true;
     var that = this;
     ripple($(that), e);
     $(that).addClass("processing");
-    setTimeout(function() {
+    setTimeout(function () {
       $(that).addClass("success");
-      setTimeout(function() {
+      setTimeout(function () {
         $app.show();
         $app.css("top");
         $app.addClass("active");
       }, submitPhase2 - 70);
-      setTimeout(function() {
+      setTimeout(function () {
         $login.hide();
         $login.addClass("inactive");
         animating = false;
@@ -39,24 +39,24 @@ $(document).ready(function() {
       }, submitPhase2);
     }, submitPhase1);
   });
-  
-  $(document).on("click", ".app__logout", function(e) {
+
+  $(document).on("click", ".app__logout", function (e) {
     if (animating) return;
     $(".ripple").remove();
     animating = true;
     var that = this;
     $(that).addClass("clicked");
-    setTimeout(function() {
+    setTimeout(function () {
       $app.removeClass("active");
       $login.show();
       $login.css("top");
       $login.removeClass("inactive");
     }, logoutPhase1 - 120);
-    setTimeout(function() {
+    setTimeout(function () {
       $app.hide();
       animating = false;
       $(that).removeClass("clicked");
     }, logoutPhase1);
   });
-  
+
 });
